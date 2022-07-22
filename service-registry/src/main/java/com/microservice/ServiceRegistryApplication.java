5919774c8ae89087d526f72570f6c0b4bcd1a627
Manish <van.manish@gmail.com>
2022-07-22 17:37:14 +0530

    added getLogs() feature in ServiceRegistry

--- service-registry/src/main/java/com/microservice/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/microservice/ServiceRegistryApplication.java
@@ -3,6 +3,9 @@ package com.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,10 @@ public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+	public void getLog(){
+		System.out.println(" --- implemented feature/branch-B -- "+Instant.now());
 	}
 
 }
