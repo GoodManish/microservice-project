eef61fa27effc14f48754c911a92c8470062fb5d
Manish <van.manish@gmail.com>
2022-07-22 19:01:43 +0530

    cloud gateway branch a changes

--- cloud-gateway/src/main/java/com/microservice/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/microservice/cloud/gateway/CloudGatewayApplication.java
@@ -1,15 +1,20 @@
 package com.microservice.cloud.gateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CloudGatewayApplication {
+public class CloudGatewayApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("cloud gateway branch A");
+	}
 }
